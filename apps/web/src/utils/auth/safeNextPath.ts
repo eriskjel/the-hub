2264d19e1971b352834f29ff679eda0644eb7ac4
Erik Skjellevik <eriskjel@stud.ntnu.no>
2025-08-16 19:51:34 +0200

    chore: add small comment explaining regex exclusion

--- apps/web/src/utils/auth/safeNextPath.ts
+++ apps/web/src/utils/auth/safeNextPath.ts
@@ -9,6 +9,7 @@ export function safeNextPath(next: string): string {
         !next.startsWith("//") &&
         next.indexOf(":") === -1 &&
         next.indexOf("\\") === -1 &&
+        // Prevent control character injection attacks by rejecting any path containing ASCII control characters.
         !/[\x00-\x1F\x7F]/.test(next)
     ) {
         return next;
