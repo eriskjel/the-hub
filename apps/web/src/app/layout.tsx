1fbf18fc9def5c63ab7d0666d2a2e9c046ad4d1a
Erik Skjellevik <98759397+eriskjel@users.noreply.github.com>
2026-02-16 19:12:54 +0100

    refactor(dependencies): add `@vercel/analytics` dependency and integrate into layout (#216)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ReactNode } from "react";
 import { Toaster } from "@/components/Toaster";
@@ -71,6 +72,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 {children}
                 <Toaster />
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
